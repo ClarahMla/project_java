d48b5fc3bf091d490350fc4de02beb0bae6a544f
ClarahMla <mlamboclarah4@gmail.com>
2025-04-19 15:36:18 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,31 @@
+function call(){
+
+
+ let text = document.getElementById("demo").value;
+
+
+let myarray = text.split(" ");
+
+document.write(myarray.length);
+
+console.log(myarray.length);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
